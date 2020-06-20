a221ce1db3c8b6cdeb1cc42058dc495013e0fbb1
codingzhengsz <62690976+codingzhengsz@users.noreply.github.com>
2020-06-20 10:39:07 +0800

    add procedure

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
-//TODO
+
+console.log(a instanceof Array); // false
+console.log(b instanceof Array); // true
 
 
 // Write a program that multiplies each entry in the following array by 2。
-var a = [1, 2, 3, 4, 5];
-// TODO should output [2,4,6,8,10]
+var a = [1, 2, 3, 4, 5
+         
+function doubleItem(item, index, arr) {
+	arr[index] = item * 2;
+}
+a.forEach(doubleItem);
+console.log(a);
 
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
-//TODO case 1 output: 'Red Green White Black'
+// case 1 output: 'Red Green White Black'
+console.log(colors.join(' '))
 // case 2 output: 'Red+Green+White+Black'
+console.log(colors.join('+'))
 // case 3 output: 'Red,Green,White,Black'
+console.log(colors.join())
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+console.log(a.sort((a , b) => {return b - a})); // [10,8,5,4,1]
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+function getTheMost(arr) {
+    var maxElement;
+    var maxCount = 1;
+    arr.reduce((p, n) = >{
+        p[n] ? p[n]++:p[n] = 1;
+        if (p[n] > maxCount) {
+            maxElement = n;
+            maxCount++;
+        }
+        return p;
+    },
+    {});
+    return maxElement;
+}
+console.log(getTheMost(arr)); // a
+
